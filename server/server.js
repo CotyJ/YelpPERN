daecd82420b5811ec9cd55f86c093a5aaebcda35
coty <Coty.Janeway@gmail.com>
2022-09-21 16:28:53 -0700

    Comments Branch

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
-require("dotenv").config();
-const port = process.env.PORT || 3000;
+// Entry point for backend application
+// Responsible for creating and initializing Express app
 
-// Express
+// Necessary for Dotenv to run. Looks for the config file .env
+require("dotenv").config();
+
+// Importing Express
 const express = require("express");
+
+// Creating an instance of Express
 const app = express();
 
+// Define the port from .env
+const port = process.env.PORT || 3000
 
-// Listening
+// Listener with PORT variable and template string.
 app.listen(3000, () => {
   console.log(`\n🌟 Listening on port 3000 🌟\n`);
 });
