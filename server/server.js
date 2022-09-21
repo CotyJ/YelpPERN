21b5ef808de3e7a9861c8730963fd33ebaad7a15
coty <Coty.Janeway@gmail.com>
2022-09-21 16:01:48 -0700

    Practicing git

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 // Entry point for backend application
 // Responsible for creating and initializing Express app
 
-require("dotenv").config();
 
 // Importing Express
 const express = require("express");
 
 // Creating an instance of Express
 const app = express();
-
-const port = process.env.PORT || 4000;
 
 // Listener
 app.listen(3000, () => {
