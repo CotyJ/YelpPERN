c1aaf6f6af81940351b482dacb523eed02871f3d
coty <Coty.Janeway@gmail.com>
2022-09-21 15:37:00 -0700

    Remove comments from Master.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,7 @@
-// Entry point for backend application
-// Responsible for creating and initializing Express app
-
-// Importing Express
 const express = require("express");
 
-// Creating an instance of Express
 const app = express();
 
-
-// Listener
 app.listen(3000, () => {
   console.log(`\n🌟 Listening on port 3000 🌟\n`);
 });
