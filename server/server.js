4fcca71fc0a08c72b94168ccd1904502de8b2d2d
coty <Coty.Janeway@gmail.com>
2022-09-21 16:00:11 -0700

    Returned comments

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
+// Entry point for backend application
+// Responsible for creating and initializing Express app
+
 require("dotenv").config();
 
+// Importing Express
 const express = require("express");
 
+// Creating an instance of Express
 const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-  console.log(`\n🌟 Listening on port ${port} 🌟\n`);
+// Listener
+app.listen(3000, () => {
+  console.log(`\n🌟 Listening on port 3000 🌟\n`);
 });
