13aa66fd38454e43835503812bb910808c794cf7
coty <Coty.Janeway@gmail.com>
2022-09-21 16:30:13 -0700

    Merging Comments

--- server/server.js
+++ server/server.js
@@ -1,19 +1,9 @@
-// Entry point for backend application
-// Responsible for creating and initializing Express app
-
-// Necessary for Dotenv to run. Looks for the config file .env
 require("dotenv").config();
 
-// Importing Express
 const express = require("express");
-
-// Creating an instance of Express
 const app = express();
-
-// Define the port from .env
 const port = process.env.PORT || 3000
 
-// Listener with PORT variable and template string.
 app.listen(3000, () => {
   console.log(`\n🌟 Listening on port 3000 🌟\n`);
 });
