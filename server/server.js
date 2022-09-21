0a5fba74b11b53a88a8da48ec5c8032e0a5890e0
coty <Coty.Janeway@gmail.com>
2022-09-21 16:07:35 -0700

    Add environment variable for PORT

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 // Entry point for backend application
 // Responsible for creating and initializing Express app
 
+// Necessary for Dotenv to run. Looks for the config file .env
+require("dotenv").config();
 
 // Importing Express
 const express = require("express");
@@ -8,7 +10,10 @@ const express = require("express");
 // Creating an instance of Express
 const app = express();
 
-// Listener
+// Define the port from .env
+const port = process.env.PORT || 3000
+
+// Listener with PORT variable and template string.
 app.listen(3000, () => {
   console.log(`\n🌟 Listening on port 3000 🌟\n`);
 });
