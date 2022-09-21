96bbb788b06b9519b3d5e8d93ce9067001ce20fa
coty <Coty.Janeway@gmail.com>
2022-09-21 16:05:27 -0700

    Fixing Git

--- server/server.js
+++ server/server.js
@@ -1,10 +1,6 @@
-require("dotenv").config();
-
 const express = require("express");
 
 const app = express();
-
-const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`\n🌟 Listening on port ${port} 🌟\n`);
