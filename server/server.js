46ae883949bf3ec83f5de6166b00d579a4a58e57
coty <Coty.Janeway@gmail.com>
2022-09-21 16:15:57 -0700

    Reorganize file w/ basic comments

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
+const port = process.env.PORT || 3000;
 
+// Express
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000
 
+
+// Listening
 app.listen(3000, () => {
   console.log(`\n🌟 Listening on port 3000 🌟\n`);
 });
