76d3b823b1f9ebbf0dd51ae773298b3c2ce7f0c5
coty <Coty.Janeway@gmail.com>
2022-09-21 17:38:46 -0700

    restore and start work again

--- server/server.js
+++ server/server.js
@@ -7,10 +7,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`\n🌟 Listening on port ${port} 🌟\n`);
 });
-
-const newFeature = "New feature";
-const new2 = '2';
-const new3 = 333;
-
-// new feature 4, will it be the only one?
-const new4 = 4444;
