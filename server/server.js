7ff99abe6753fab44c8960a976d16531bae2715a
coty <Coty.Janeway@gmail.com>
2022-09-21 17:09:03 -0700

    starting over

--- server/server.js
+++ server/server.js
@@ -7,12 +7,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`\n🌟 Listening on port ${port} 🌟\n`);
 });
-
-const newfeature = "New Feature!";
-const newfeature2 = 'new 2!';
-const newfeature3 = 3;
-const newFeature4 = 4444;
-const newfeature5 = 'five';
-
-// New feature 6. All other comments should still work for previous features (not remove 5)
-const newFeature6 = 'sixsixsix';
